28b727a3c432ca024f6035619b104ae7a0d7d679
TamTania <solovei_tania@ukr.net>
2016-12-27 13:36:57 +0200

    task 1 done

--- task1/js/scripts.js
+++ task1/js/scripts.js
@@ -12,6 +12,41 @@ Item.prototype.getWeight = function () {
 	return this.weight;
 };
 
+console.log("1. Press 'Generate items offer' button");
+console.log("2. Press 'Weigh products' button");
+console.log("3. Press 'Evaluate' button");
+console.log("4. Nice! Get bill");
+
+function selectPlace () {
+	var places = ['electronic', 'bodycare', 'backery', 'butchery', 'frozen', 'dairy', 'grocery', 'beer&wine'];
+  	var place = places[Math.floor(Math.random()*places.length)];
+  	return place;
+};
+
+
+
+function evaluation () {
+  var price = Math.round(Math.random()*500);
+  return price;
+};
+
+function weigh () {
+	var weight = Math.round(Math.random()*20);
+  	return weight;
+};
+
 function purchase () {
-	
-}
+	var purchases = [];
+	var billValue = 0;
+	for (var i = 0; i < 15; i++) {
+	    purchases.push(new Item(selectPlace(), evaluation(), weigh()));
+	    console.log(purchases[i]);
+	    billValue += purchases[i].getPrice();
+	}
+	console.log("Total: " + billValue + " $");
+};
+
+
+
+
+
